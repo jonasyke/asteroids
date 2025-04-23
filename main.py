42c7d0478e86f5d72338f265978f4ba9416bb7a8
jonasyke <jonasyke171@gmail.com>
2025-04-22 16:36:55 -0800

    L4: Sprites

--- main.py
+++ main.py
@@ -11,13 +11,17 @@ def main():
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
-    while True:
-        screen.fill((0,0,0))
-        pygame.display.flip()
+    clock = pygame.time.Clock()
+    dt = 0
 
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        screen.fill((0,0,0))
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000.0
 
 if __name__ == "__main__":
     main()
