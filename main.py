42988af241fc969f780c8d3a073959ae5c12d5b4
jonasyke <jonasyke171@gmail.com>
2025-04-24 15:22:57 -0800

    L5: Added Kill

--- main.py
+++ main.py
@@ -45,6 +45,12 @@ def main():
             if ast.collision(player):
                 print("Game over!")
                 return
+            
+        for ast in asteroids:
+            for bullet in shots:
+                if ast.collision(bullet):
+                    ast.kill()
+                    bullet.kill()
 
         screen.fill("black")
     
